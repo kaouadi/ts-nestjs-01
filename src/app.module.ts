0671762e409de44d21df7e53f7ad171ef1aac781
Karim Aouadi <kdolphinus@gmail.com>
2020-04-13 19:43:32 +0200

    Added midlleware log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/cats.controller'
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply((req, res, next) => {
+        console.log(`Request...`);
+        next();
+      })
+      .forRoutes(
+                 { path: 'cats', method: RequestMethod.GET },
+                 { path: 'cats/:id', method: RequestMethod.GET }
+                );
+  }
+}
